2b2c2f224359ffc8b0438cefa9e8d1381df6ca6c
geebe <geebee3xx@outlook.com>
2018-07-03 18:33:56 +0200

    Meine Django Girls App, erster Commit

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+'''
+Created on 03.07.2018
+
+@author: geebe
+'''
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
---
+++ blog/models.py
@@ -0,0 +1,24 @@
+'''
+Created on 03.07.2018
+
+@author: geebe
+'''
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
